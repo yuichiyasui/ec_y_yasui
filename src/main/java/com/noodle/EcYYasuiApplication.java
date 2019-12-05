c5df83cdc5a9a9807feb1a005dc29fcbe6d83bc8
yuichiyasui <yassan.b.a.w@gmail.com>
2019-12-05 11:12:57 +0900

    first commit

---
+++ src/main/java/com/noodle/EcYYasuiApplication.java
@@ -0,0 +1,13 @@
+package com.noodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcYYasuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcYYasuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noodle/EcYYasuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcYYasuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
